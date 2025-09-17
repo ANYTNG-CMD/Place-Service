efac739d9574c410f931defac5933a7ead6c9672
Dimas Rocha <dimas.dasp@gmail.com>
2025-09-16 22:33:19 -0300

    place Test

--- src/test/java/br/com/APIdm/PlaceServiceAPI/PlaceServiceApiApplicationTests.java
+++ src/test/java/br/com/APIdm/PlaceServiceAPI/PlaceServiceApiApplicationTests.java
@@ -1,14 +1,37 @@
 package br.com.APIdm.PlaceServiceAPI;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import api.PlaceRequest;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PlaceServiceApiApplicationTests {
+	@Autowired
+	WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreatePlaceSuccess() {
+		var name = "Valid Name";
+		var state = "Valid State";
+		var slug = "valid_name"
+		
+		webTestClient
+		.post()
+		.uri("/places")
+		.bodyValue(new PlaceRequest(name, state))
+		.exchange()
+		.expectBody()
+		.jsonPath("name").isEqualTo(name)
+		.jsonPath("state").isEqualTo(state)
+		.jsonPath("slug").isEqualTo(slug)
+		.jsonPath("createdAt").isNotEmpty()
+		.jsonPath("updateAt").isNotEmpty()
+
+		
 	}
 
 }
